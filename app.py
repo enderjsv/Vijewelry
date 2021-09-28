d036d88ed841517e30b2e1caa4d47abb4d6a25f5
DHURD <enderjsv@hotmail.com>
2021-09-28 10:43:29 -0700

    trying db connect

--- app.py
+++ app.py
@@ -14,26 +14,26 @@ config_file = open("config.txt", "r")
 os.environ['AZURE_APP_CONFIG_CONNECTION_STRING'] = config_file.read()
 
 connection_string = os.getenv('AZURE_APP_CONFIG_CONNECTION_STRING')
-# app_config_client = AzureAppConfigurationClient.from_connection_string(
-#    connection_string)
-# params = quote_plus(app_config_client.get_configuration_setting(
-#    key='db_connection_string').value)
+app_config_client = AzureAppConfigurationClient.from_connection_string(
+    connection_string)
+params = quote_plus(app_config_client.get_configuration_setting(
+    key='db_connection_string').value)
 
-#db = create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))
+db = create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))
 
 
 @app.route("/")
 def index():
 
-    #sql = text("select * from test_table")
-    #result = db.execute(sql)
-    #result_string = ""
+    sql = text("select * from test_table")
+    result = db.execute(sql)
+    result_string = ""
 
-    # for row in result:
-    #    result_string += row[0] + " "
+    for row in result:
+        result_string += row[0] + " "
 
-    return "The connection string is " + connection_string
-    # return (db.table_names()[0]+" "+result_string)
+    # return "The connection string is " + connection_string
+    return (db.table_names()[0]+" "+result_string)
     # return render_template("index.html")
 
 
